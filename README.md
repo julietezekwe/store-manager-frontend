d088d680f1af6fc72598aa98239304b39c47c5e7
chidimma Ezekwe <julietezekwe@gmail.com>
2019-02-22 00:43:45 +0100

    Chore(setups) Setup the application folders Setup testing framework Setup continous integration Setup linting library Setup react/redux Setup webpack configuration Initialize the application Setup code coverage [Chore #162980053]

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # store-manager-frontend
+
+[![Build Status](https://travis-ci.org/julietezekwe/store-manager-frontend.svg?branch=develop)](https://travis-ci.org/julietezekwe/store-manager-frontend?branch=develop) [![Test Coverage](https://api.codeclimate.com/v1/badges/fd3a96953d9d7eccaefb/test_coverage?branch=develop)](https://codeclimate.com/github/julietezekwe/store-manager-frontend/test_coverage?branch=develop)
---
+++ src/__tests__/setup/setUp.config.js
@@ -0,0 +1,30 @@
+import 'babel-polyfill';
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+
+global.mount = mount;
+global.shallow = shallow;
+global.React = React;
+
+class LocalStorageMock {
+  constructor() {
+    this.store = {};
+  }
+
+  clear() {
+    this.store = {};
+  }
+
+  getItem(key) {
+    return this.store[key] || null;
+  }
+
+  setItem(key, value) {
+    this.store[key] = value;
+  }
+
+  removeItem(key) {
+    delete this.store[key];
+  }
+}
+global.localStorage = LocalStorageMock;
